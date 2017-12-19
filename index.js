10ae3ac0839b9c653c16328557f38fd725caf114
Brian Embry <b.a.embry@gmail.com>
2017-12-19 17:47:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector) {
+  var firstMatch = document.querySelector(selector)
+  return firstMatch
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('.ranked-list')
+  
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n)
+  }
+}
+
+function deepestChild() {
+  const grandNode = document.querySelector('#grand-node').querySelectorAll('div')
+  
+  for (let i = 0; i < grandNode.length; i++) {
+    
+  }
+}
